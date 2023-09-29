376085cb448be3d18f606955f478ff3132dc45a7
Z8-28 <josealejandroquiroa@gmail.com>
2023-09-28 18:15:46 -0600

    hola

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Importamos los componentes
+import CompShowBlogs from './blog/ShowBlogs.js';
+import CompCreateBlog from './blog/CreateBlog.js';
+import CompEditBlog from './blog/EditBlog.js';
+//Importamos el router
+
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <CompShowBlogs/>} />
+          <Route path='/create' element={ <CompCreateBlog/>} />
+          <Route path='/edit/:id' element={ <CompEditBlog/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
